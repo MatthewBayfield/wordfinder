ea8551c6f9be74f50d41c42ca95897edd3dec6a1
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-03-23 21:27:15 +0000

    Add click event listeners for the close/game_instruction button for opening and closing the how to play window.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Event listeners to allow the how_to_play window to be opened and closed using the relevant buttons.
+
+let howToPlayWindow = document.getElementById('how_to_play_window');
+let closeButton = howToPlayWindow.children[2];
+closeButton.addEventListener('click', function (event) {
+    howToPlayWindow = document.getElementById('how_to_play_window');
+    howToPlayWindow.style.display = 'none';
+})
+
+document.getElementById('game_instructions').addEventListener('click', function (event) {
+    howToPlayWindow = document.getElementById('how_to_play_window');
+    howToPlayWindow.style.display = 'block';
+})
