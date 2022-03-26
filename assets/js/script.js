5b3c42cd10d91bcd92f9aa4958ef7850fabde2ed
MatthewBayfield <matthew.bayfield@yahoo.com>
2022-03-26 11:59:12 +0000

    Create timer adjuster function to give the timer its timer functionality. Modify the start button event listener to trigger starting/stopping/reseting the timer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,15 +36,21 @@ function soundMode() {
 let gameStartSound = new Audio("assets/audio/game_start.mp3");
 
 
-//start button click event listener. Changes button to a quit button.
+//start button click event listener. Changes button to a quit button, and a quit button to start button. Triggers start game sound and starts/resets clock.
 document.getElementById('main_game_area').children[1].addEventListener('click', function (event) {
     if (this.textContent === 'Start') {
         this.textContent = 'Quit';
         if (soundMode()) {
             gameStartSound.play();
         }
+        if (!(document.getElementById('no_timer').checked)) {
+            timer = setInterval(timer_Adjuster, 1000);
+        }
+
     } else {
         this.textContent = 'Start';
+        clearInterval(timer);
+        setTimer();
     }
 })
 
@@ -68,4 +74,28 @@ for (let input of document.querySelectorAll('[name=timer]')) {
     input.addEventListener('click', function (event) {
         setTimer();
     })
+}
+
+/**Gives the timer its timer functionality: decreases by 1 every 1s until 0 when it resets.
+ */
+function timer_Adjuster() {
+    let timeInSeconds = (Number(document.getElementsByClassName('sidebar')[0].children[0].children[1].textContent.slice(0, 2))) * 60 +
+        Number(document.getElementsByClassName('sidebar')[0].children[0].children[1].textContent.slice(3, 5));
+    if (timeInSeconds > 0) {
+        timeInSeconds -= 1;
+
+        let minutes = Math.floor(timeInSeconds / 60);
+        let timerSeconds = timeInSeconds % 60;
+        if (`${timerSeconds}`.length === 2) {
+            let strTime = `0` + `${minutes}` + `:` + `${timerSeconds}`;
+            document.getElementsByClassName('sidebar')[0].children[0].children[1].textContent = strTime;
+        } else {
+            let strTime = `0` + `${minutes}` + `:` + `0` + `${timerSeconds}`;
+            document.getElementsByClassName('sidebar')[0].children[0].children[1].textContent = strTime;
+        }
+    } else {
+        clearInterval(timer);
+        setTimer();
+    }
+
 }
